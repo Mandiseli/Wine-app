e9d0de3f87547ee6c3870871f03ed853facdead2
Mandiseli <mandiseli@outlook.com>
2024-02-19 15:45:44 +0200

    First Commit

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./login.css";
+import user_icon from "../../assets/person.png"
+import email_icon from "../../assets/email.png"
+import password_icon from "../../assets/password.png"
+
+const Login = () => {
+  return (
+    <div className='login-container'>
+
+        <div className="login-header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+
+        <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" />
+        </div>
+
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" />
+        </div>
+
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" />
+        </div>
+        </div>
+        <div className="forgot-password">Lost Password? <span>Click Here!</span> </div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Login</div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
